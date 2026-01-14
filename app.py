0cdef1cfac72c18e5337321ab4500b82f4619ed1
TSK <patrickjhemming@icloud.com>
2026-01-15 03:04:04 +1100

    Update app.py

--- app.py
+++ app.py
@@ -512,7 +512,7 @@ class PumpFunMonitor:
                 "order": "DESC"
             }
             
-            async with self.session.get(url, params=params, timeout=15) as resp:
+            async with self.session.get(url, params=params, timeout=15, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}  as resp:
                 resp.raise_for_status()
                 data = await resp.json()
                 
